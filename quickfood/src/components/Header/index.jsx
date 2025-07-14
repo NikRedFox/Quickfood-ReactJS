d1c1e672abfcb1ad97b2a71b4042c7d7e202038e
Nikolas <nikolasbs11@gmail.com>
2025-07-14 16:22:05 -0300

    Feat: Added some adjustments for mobile compatibility. Just on header for now

--- quickfood/src/components/Header/index.jsx
+++ quickfood/src/components/Header/index.jsx
@@ -2,24 +2,44 @@ import './style.css'
 import Logo from '../../assets/QUICKFOOD-Icon.svg'
 import Search from '../../assets/Search-icon.svg'
 import Cart from '../../assets/Shopcart-icon.svg'
+import { useState } from 'react'
 
 
 const Header =({darkTheme, setDarkTheme})=>{
+    const[menuOpen, setMenuOpen] =useState(false)
+
     return(
         <header>
             <div className="header_icon">
                 <img src={Logo} alt="Logo empresa" title="Logo empresa"/>
             </div>
 
-            <nav>
+            <button className='hamburguer' onClick={() => setMenuOpen(!menuOpen)}>☰</button>
+            
+            <nav className={`dropdown-menu ${menuOpen ? 'open' : ''}`}>
                 <ul className="header-links">
                     <li><a href="#" className="header-item"><span className="green-text">Home</span></a> </li>             
                     <li><a href="#" className="header-item">Product</a></li>
                     <li><a href="#" className="header-item">Contact us</a></li>
                     <li><a href="#" className="header-item">About</a></li>          
-                </ul>            
-            </nav>
+                </ul>  
 
+                <div className="dropdown-buttons">
+                    <div className="header-button-iconbox">
+                        <button className="header-button-icon">
+                            <img src={Search} alt="Search icon" title="Search icon" />
+                        </button>
+                        <button className="header-button-icon">
+                            <img src={Cart} alt="Shopcart icon" title="Shopcart icon"/>
+                        </button>
+                    </div>            
+                    
+                    <button className="header-button-login">Login</button>
+                    <button className='theme-toggle' onClick={() => setDarkTheme((prev) => !prev)}>{darkTheme ? '🌙' : '☀️'}</button>            
+                </div>
+
+            </nav>
+            
             <div className="header-button-container">
                 <div className="header-button-iconbox">
                     <button className="header-button-icon">
@@ -33,6 +53,9 @@ const Header =({darkTheme, setDarkTheme})=>{
                 <button className="header-button-login">Login</button>
                 <button className='theme-toggle' onClick={() => setDarkTheme((prev) => !prev)}>{darkTheme ? '🌙' : '☀️'}</button>
             </div>
+
+            
+            
         </header>
     )
 }
